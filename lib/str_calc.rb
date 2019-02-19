63cdcfeb03aa5940e3c465893d3dfeae66a9468f
Luca <eto.lucafrancesco@gmail.com>
2019-02-19 12:09:15 +0000

    test passing

--- lib/str_calc.rb
+++ lib/str_calc.rb
@@ -2,7 +2,7 @@ class StringCalculator
 
   def add(str)
     return 0 if str.empty?
-    numbers = str.split(',').map! { |number| number.to_i }
+    numbers = str.gsub(/\n/, ',').split(',').map! { |number| number.to_i }
     return numbers.inject(:+)
   end
 
