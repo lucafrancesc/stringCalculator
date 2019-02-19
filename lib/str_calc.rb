870e714866695e7ecac48c0c72341a7ec2397b5a
Luca <eto.lucafrancesco@gmail.com>
2019-02-19 12:43:12 +0000

    all test passing

--- lib/str_calc.rb
+++ lib/str_calc.rb
@@ -4,6 +4,7 @@ class StringCalculator
     return 0 if str.empty?
     return '-,\n- is not a valid syntax' if /,\n/.match(str)
     numbers = str.gsub(/\n/, "#{delimiter(str)}").split("#{delimiter(str)}").map! { |number| number.to_i }
+    return 'negative not allowed' if numbers.sort[0] < 0
     return numbers.inject(:+)
   end
 
