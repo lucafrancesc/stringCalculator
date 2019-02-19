0f39a49f3cc9b254af8cb809da53bbe3b995fdd6
Luca <eto.lucafrancesco@gmail.com>
2019-02-19 11:53:00 +0000

    second test passing / extra test written

--- lib/str_calc.rb
+++ lib/str_calc.rb
@@ -1,4 +1,10 @@
 class StringCalculator
   def add(str)
     return 0 if str.empty?
+    numbers = str.split(',')
+    numbers.map! do |number|
+      number.to_i
+    end
+    return numbers.inject(:+)
+  end
 end
--- spec/str_calc_spec.rb
+++ spec/str_calc_spec.rb
@@ -12,5 +12,9 @@ describe StringCalculator do
     it 'returns the sum of the number in the string passed' do
       expect(calc.add('1')).to eq 1
     end
+
+    it 'returns the sum of the number in the string passed' do
+      expect(calc.add('1,3,4')).to eq 8
+    end
   end
 end
