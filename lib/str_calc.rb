46ca9af43310697fd484fb0231cf0f1a720a42a3
Luca <eto.lucafrancesco@gmail.com>
2019-02-19 13:35:18 +0000

    further ref, all test passing

--- lib/str_calc.rb
+++ lib/str_calc.rb
@@ -10,16 +10,16 @@ class StringCalculator
 
   def delimiter(str)
     str[0..1] == '//' ? (del = delimiter_modifier(str)) : (del = ',')
-    return del
+    del
   end
 
   def delimiter_modifier(str)
-    return (str[2] == '[') && (/]/.match(str)) ? str[str.index('[')+1..str.index(']')-1] : str[2]
+    (str[2] == '[') && (/]/.match(str)) ? str[str.index('[')+1..str.index(']')-1] : str[2]
   end
 
   def array_checker(numbers)
-    under_1000 = []
-    numbers.each { |number| under_1000.push(number) if number < 1000 }
-    return under_1000.inject(:+)
+    sum = 0
+    numbers.each { |number| sum += number if number < 1000 }
+    sum
   end
 end
